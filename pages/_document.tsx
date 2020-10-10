cb4089e9596416dd7c88b35a8d2d1e493d30a53c
Arif <arifkavas@gmail.com>
2020-10-10 14:38:22 +0200

    Lighthouse fixes for SEO and best practices

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -16,6 +16,16 @@ class MyDocument extends Document {
             sizes='96x96'
             href='./favicon-96x96.png'
           />
+          <title>Marley Spoon recipes</title>
+          <meta charSet='UTF-8' />
+          <meta
+            property='og:title'
+            content='Marley Spoon recipes'
+            key='title'
+          />
+          <meta name='description' content='A recipe list' />
+          <meta name='keywords' content='Marley Spoon, Delicious, Food' />
+          <meta name='author' content='Marley Spoon' />
         </Head>
         <body>
           <Main />
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,22 @@
 import { Component } from 'react';
 import Router from 'next/router';
+import { Head } from 'next/document';
 
 class Home extends Component {
   componentDidMount() {
     Router.push('/recipeList');
   }
   render() {
-    return null;
+    return (
+      <div>
+        <Head>
+          <meta
+            name='viewport'
+            content='initial-scale=1.0, width=device-width'
+          />
+        </Head>
+      </div>
+    );
   }
 }
 
